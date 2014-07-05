ad3fe14c2edf4637d1b154ac59a1fa7498dc15c6
XueFei Yang <yangxuefei@xfyang-MacBook-Pro.local>
2014-07-05 17:54:22 -0300

    Adding Various Size Block Package
    Adding Various Size Block Package

---
+++ src/main/java/com/kbg/VariousSizeBlock/Basic.java
@@ -0,0 +1,32 @@
+/* This package is to set up Various Size of Block.
+ * A 1x1 size block, a 3x3 size block and a 9x9 size block
+ * 
+ * @Author XueFei Yang
+ */
+
+package com.kbg.VariousSizeBlock;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+@Mod(modid = Basic.MODID, version = Basic.VERSION)
+public class Basic {
+	public static final String MODID = "basic";
+	public static final String VERSION = "1.0";
+	public static Block basicBlock = new BasicBlock(Material.rock).setCreativeTab(CreativeTabs.tabBlock).setBlockName("basicBlock").setLightLevel(1.0f);
+	
+	@EventHandler
+	public void preInit(FMLInitializationEvent event){
+		GameRegistry.registerBlock(basicBlock, "Basic Block");
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event){
+		
+	}
+}
---
+++ src/main/java/com/kbg/VariousSizeBlock/BasicBlock.java
@@ -0,0 +1,13 @@
+package com.kbg.VariousSizeBlock;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BasicBlock extends Block{
+
+	protected BasicBlock(Material p_i45394_1_) {
+		super(p_i45394_1_);
+		
+	}
+
+}
