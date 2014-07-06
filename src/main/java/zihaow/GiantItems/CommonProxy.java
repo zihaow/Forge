260ad7b0723087ecd91c8d97a6f2f25a653df3be
Zihao Wu <zihao.wu1993@gmail.com>
2014-07-06 18:20:11 -0300

    Update CommonProxy.java

--- src/main/java/zihaow/GiantItems/CommonProxy.java
+++ src/main/java/zihaow/GiantItems/CommonProxy.java
@@ -2,6 +2,11 @@ package zihaow.GiantItems;
 
 import net.minecraft.world.World;
 
+/**
+ * CommonProxy class,to make the GiantItems mod working on Minecraft.
+ * @author: Zihao Wu.
+*/
+
 public class CommonProxy
 {
 	public void registerClientInformation(){}
