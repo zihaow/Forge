79b7a03939d638e265c59872b2bb2d627a9e9124
Zihao Wu <zihao.wu1993@gmail.com>
2014-07-09 14:23:22 -0300

    model class for wolverine

--- src/main/java/net/minecraft/src/ModelWolverine.java
+++ src/main/java/net/minecraft/src/ModelWolverine.java
@@ -16,7 +16,7 @@ import net.minecraft.util.MathHelper;
 
 public class ModelWolverine extends ModelQuadruped{
 	
-	public ModelRenderer head = new ModelRenderer(this, 0, 0);
+    public ModelRenderer head = new ModelRenderer(this, 0, 0);
     public ModelRenderer body;
     public ModelRenderer leg1;
     public ModelRenderer leg2;
@@ -26,9 +26,9 @@ public class ModelWolverine extends ModelQuadruped{
     protected float field_78151_h = 5.0F;
     private static final String __OBFID = "CL_00000851";
 
-	  public ModelWolverine(int par1, float par2){
-		  super(par1, par2);
-		  this.head.addBox(-4.0F, -4.0F, -8.0F, 8, 8, 8, par2);
+    public ModelWolverine(int par1, float par2){
+      super(par1, par2);
+      this.head.addBox(-4.0F, -4.0F, -8.0F, 8, 8, 8, par2);
       this.head.setRotationPoint(0.0F, (float)(18 - par1), -6.0F);
       this.body = new ModelRenderer(this, 28, 8);
       this.body.addBox(-5.0F, -10.0F, -7.0F, 10, 16, 8, par2);
@@ -45,9 +45,9 @@ public class ModelWolverine extends ModelQuadruped{
       this.leg4 = new ModelRenderer(this, 0, 16);
       this.leg4.addBox(-2.0F, 0.0F, -2.0F, 4, par1, 4, par2);
       this.leg4.setRotationPoint(3.0F, (float)(24 - par1), -5.0F);
-	  }
+    }
 
-	  /**
+    /**
      * Sets the models various rotation angles then renders the model.
      */
     public void render(Entity par1Entity, float par2, float par3, float par4, float par5, float par6, float par7){
