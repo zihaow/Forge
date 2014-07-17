a76c3951429e147951fcf3c5043bf4d0a7abb7c1
zihaow <zihao.wu1993@gmail.com>
2014-07-17 14:04:01 -0300

    Main class for our bad ass character
    Main class for our bad ass character

---
+++ src/main/java/zihaow/mobs/entity/BadAss.java
@@ -0,0 +1,96 @@
+package zihaow.mobs.entity;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMoveThroughVillage;
+import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeModContainer;
+
+/**
+ * BadAss class set the feature of our badAss.
+ * @author Zihao Wu.
+ */
+
+public class BadAss extends EntityMob {
+
+	/**
+	 * Constructor to initialize the bad ass.
+	 * @param world
+	 */
+	public BadAss(World world){
+		super(world);
+		this.getNavigator().setBreakDoors(true);
+		this.tasks.addTask(0, new EntityAISwimming(this));
+		this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, 0.5D, true));
+		this.tasks.addTask(4, new EntityAIAttackOnCollide(this, EntityVillager.class, 0.5D, true));
+		this.tasks.addTask(6, new EntityAIMoveThroughVillage(this, 0.5D, true));
+		this.tasks.addTask(7, new EntityAIWander(this, 1.0D));
+		this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 10.0F));
+		this.tasks.addTask(8, new EntityAILookIdle(this));
+		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityVillager.class, 0, true));
+		this.setSize(1.6F, 2.8F);
+	}
+
+	//to apply bad ass's attribute
+	protected void applyEntityAttributes(){
+		super.applyEntityAttributes();
+		this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(40.0D);
+		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(2.2D);
+		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(6.0D);
+	}
+
+	//to enable AI.
+	protected boolean isAIEnabled(){
+		return true;
+	}
+
+	protected String getLivingSound()
+	{
+		return "mob.zombie.say";
+	}
+
+
+	protected String getHurtSound()
+	{
+		return "mob.zombie.hurt";
+	}
+
+
+	protected String getDeathSound()
+	{
+		return "mob.BadAss-death";
+	}
+
+	protected void func_145780_a(int p_145780_1_, int p_145780_2_, int p_145780_3_, Block p_145780_4_)
+	{
+		this.playSound("mob.zombie.step", 0.15F, 1.0F);
+	}
+
+	protected Item getDropItem()
+	{
+		return Items.bone;
+	}
+
+	public EnumCreatureAttribute getCreatureAttribute()
+	{
+		return EnumCreatureAttribute.UNDEAD;
+	}
+
+}
