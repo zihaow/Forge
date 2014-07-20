d7ef169bd32497f9b8646c59740e9206b97ee90a
Zihao Wu <zihao.wu1993@gmail.com>
2014-07-20 12:17:28 -0300

    Remove unnecessary code and applied javadoc

--- src/main/java/zihaow/mobs/common/CommonProxy.java
+++ src/main/java/zihaow/mobs/common/CommonProxy.java
@@ -1,29 +1,13 @@
 package zihaow.mobs.common;
 
-//import cpw.mods.fml.client.registry.RenderingRegistry;
-//import cpw.mods.fml.common.network.IGuiHandler;
-//import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
-
+/**
+ * CommonProxy class.
+ * @author: Zihao Wu.
+ */
+ 
 public class CommonProxy {
-	//public void registerRenderInformation(){}
-	public void registerRenderThings(){
-		
-	}
-	 /**
-	@Override
-	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z){
-		return null;
-	}
-		 
-	@Override
-	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z){
-		return null;
-	}
-		
-	public static int addArmor(String name){
-		return 0;
-	}
-	*/
+	
+	public void registerRenderThings(){}
 }
